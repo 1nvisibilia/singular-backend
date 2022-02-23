354e1710ccd1c57b3cf81427e51a3002dd7aeec9
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2022-02-22 20:35:35 -0500

    connects sockets; resolves CORS issues between frontend and backend

--- api/routes.js
+++ api/routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-
+router.get("/api/sup", (req, res) => {
+	res.send("lmfao");
+})
 
 router.get("/", (req, res) => {
 	res.send("sup");
 });
-
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+// Create the express server
 const express = require("express");
-const server = express();
+const app = express();
+const cors = require("cors");
 const routes = require("./api/routes");
-const PORT = process.env.PORT || 9000;
 
-server.use("/", routes);
+// Creating the Http socker server
+const http = require("http");
+const server = http.createServer(app);
+const { Server: SocketServer } = require("socket.io");
+const frontEndURL = "http://localhost:8000";
+const PORT = process.env.PORT || 9000;
+const io = new SocketServer(server, {
+	cors: {
+		origin: frontEndURL
+	}
+});
+
+
+// Setting up the routes
+app.use("/", routes);
+
+// Allowing for Cross-origin Access
+app.use(cors());
+
+io.on('connection', (socket) => {
+	console.log(socket);
+	console.log('a user connected');
+});
 
 server.listen(PORT, () => {
-	console.log("Server starting on http://localhost:9000");
+	console.log("Server starting on http://localhost:" + PORT);
 });
